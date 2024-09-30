e18cd78628163dfab1e1f9f29a472dd7c7245326
idk <stavivgi0@gmail.com>
2024-09-30 12:30:21 +0300

    create a green background

--- main.py
+++ main.py
@@ -1,44 +1,7 @@
-from pygame import *
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-green= (64,200,45)
-screen = display.set_mode((800, 600))
-
-
-running = True
-
-while running:
-    screen.fill(green)
-    display.flip()
-for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
 
 
 
 
-
-
-
-# import pygame
-#
-# pygame.init()
-#
-# SCREEN_WIDTH = 800
-# SCREEN_HEIGHT = 600
-#
-# screen = pygame.display.set_mode((800, 600))
-#
-# pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-#
-# running = True
-#
-# while running:
-#
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             running = False
 
 # import consts
 # from consts import *
